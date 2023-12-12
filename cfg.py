ce2c967f4555f068e4bad346044561c14f24868c
andreawiech <118971544+andreawiech@users.noreply.github.com>
2023-12-12 22:54:56 +0100

    Add files via upload
    These is just the base version of the package with safety check if the wrf output file in the cfg.py exist and some changes in core.py , graphics.py so that the package can deal with 2dim variables.

---
+++ cfg.py
@@ -0,0 +1,21 @@
+""" Configuration module containing settings and constants. """
+import os
+import sys
+
+#wrfout = '/home/c707201/temp/WRF_output_project.nc'
+wrfout = r'C:\Users\andre\Downloads\WRF_output_project.nc'
+
+if os.path.isfile(wrfout):
+    
+    # location of data directory
+    pkgdir = os.path.dirname(__file__)
+    html_template = os.path.join(pkgdir, 'data', 'template.html')
+    test_ts_df = os.path.join(pkgdir, 'data', 'test_df_timeseries.pkl')
+    test_hgt = os.path.join(pkgdir, 'data', 'test_hgt.nc')
+
+    # minimum and maximum elevations for topography plot
+    topo_min = 0
+    topo_max = 3200
+else:
+    print('The specified WRF output file does not exist. Please set a valid path in cfg.py.')
+    sys.exit(1)
