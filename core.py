ea3521de9038041f3e4c981090689f7fba9c5a44
andreawiech <118971544+andreawiech@users.noreply.github.com>
2024-01-04 22:37:24 +0100

    Update core.py

--- core.py
+++ core.py
@@ -10,7 +10,7 @@ import xarray as xr
 
 from wrfvis import cfg, grid, graphics, skewT
 
-def get_wrf_timeseries(param, lon, lat, zagl, rad):
+def get_wrf_timeseries(param, lon, lat, zagl, rad = 0):
     """Read the time series from the WRF output file.
     
     Parameters
@@ -34,72 +34,171 @@ def get_wrf_timeseries(param, lon, lat, zagl, rad):
         WRF topography
     col_names: list
         contains column names of all gridcells in the target radius
+        empty if radius smaller then nearest gridcell distance
     """
 
     with xr.open_dataset(cfg.wrfout) as ds:
         # find nearest grid cell
         ngcind, ngcdist = grid.find_nearest_gridcell(
                           ds.XLONG[0,:,:], ds.XLAT[0,:,:], lon, lat)
+        # convert binary times to datetime
+        wrf_time = pd.to_datetime(
+                   [bytes.decode(time) for time in ds.Times.data], 
+                   format='%Y-%m-%d_%H:%M:%S') 
+        # replace time coordinate (1-len(time)) with datetime times
+        ds = ds.assign_coords({'Time': wrf_time})
         
-        # find grid cells that lie inside specified radius
-        gcind_inrad = grid.find_grid_cells_in_radius(ngcind, ngcdist, rad, lon, lat, ds)
-        
-        # check if variable is 3-dim
-        # variable is 3-dim if dimensions of dataframe variable >= 4
-        if len(ds[param].dims)>=4:
-            
-            # find nearest vertical level
-            nlind, nlhgt = grid.find_nearest_vlevel(
-                       ds[['PHB', 'PH', 'HGT', param]], ngcind, param, zagl)
-
-            # convert binary times to datetime
-            wrf_time = pd.to_datetime(
-                       [bytes.decode(time) for time in ds.Times.data], 
-                       format='%Y-%m-%d_%H:%M:%S') 
-            # replace time coordinate (1-len(time)) with datetime times
-            ds = ds.assign_coords({'Time': wrf_time})
-
-            # extract time series
-            if param == 'T':
-                # for 3-dim varibale T (perturbation potential Temperature) add 300 to get potential Temperature
-                # create an array with values equal to zero 
-                # with shape number of grids inside radius x 36 (time) x 36 (bottom_top)
-                vararray = np.zeros((len(gcind_inrad),36,36))
-                n = 0
-                # Loop to write values of each grid in an array
-                for i in gcind_inrad:
-                    vararray[n] = ds[param][np.arange(len(ds.Time)), nlind, i[0], i[1]] + 300
-                    n = n + 1
-            else:
-                # for all other 4-dim variables 
-                vararray = np.zeros((len(gcind_inrad),36,36))
-                n = 0
-
-                for i in gcind_inrad:
-                    vararray[n] = ds[param][np.arange(len(ds.Time)), nlind, i[0], i[1]]
-                    n = n + 1
-            
-            # create column lables needed for DataFrame
-            col_names = []
-            for i in range(len(gcind_inrad)):
-                col_names = col_names + [f'Grid{i}'] 
+        # If no radius is given or radius is smaller than nearest gridcell distance
+        if rad <= ngcdist:
+            # check if variable is 3-dim
+            # variable is 3-dim if dimensions of dataframe variable >= 4
+            if len(ds[param].dims)>=4:
                 
-            # write for each Grid the variables at hight level 0 to one DataFrame
-            df = pd.DataFrame()
-            
-            for i in range(len(gcind_inrad)):
-                dfx = pd.DataFrame(data=vararray[i,:,0],
-                                   index=["1","2", "3","4","5","6","7","8","9","10"
-                                          ,"11","12","13","14","15","16","17","18","19","20"
-                                          ,"21","22","23","24","25","26","27","28","29","30"
-                                          ,"31","32","33","34","35","36"], 
-                                   columns=[col_names[i]], dtype=None, copy=None)
-                df = pd.concat([df,dfx],axis=1)
+                # find nearest vertical level
+                nlind, nlhgt = grid.find_nearest_vlevel(
+                               ds[['PHB', 'PH', 'HGT', param]], ngcind, param, zagl)
+                # extract time series
+                if param == 'T':
+                    # WRF output is perturbation potential temperature
+                    vararray = ds[param][np.arange(len(ds.Time)), nlind, ngcind[0], ngcind[1]] + 300
+                else:
+                    vararray = ds[param][np.arange(len(ds.Time)), nlind, ngcind[0], ngcind[1]]
+                df = vararray[:,0].to_dataframe()
+    
+                # add information about the variable
+                df.attrs['variable_name'] = param
+                df.attrs['variable_units'] = ds[param].units
+    
+                # add information about the location
+                df.attrs['distance_to_grid_point'] = ngcdist
+                df.attrs['lon_grid_point'] = ds.XLONG.to_numpy()[0, ngcind[0], ngcind[1]]
+                df.attrs['lat_grid_point'] = ds.XLAT.to_numpy()[0, ngcind[0], ngcind[1]]
+                df.attrs['grid_point_elevation_time0'] = nlhgt[0]
+    
+                # terrain elevation
+                wrf_hgt = ds.HGT[0,:,:]
+                
+                # col_names have to be returned that the number of returned values stays same
+                col_names = []
+                
+                return df, wrf_hgt, col_names
+        
+            # extract time series
+            vararray = ds[param][np.arange(len(ds.Time)), ngcind[0], ngcind[1]]
+            df = vararray[:].to_dataframe()
 
             # add information about the variable
             df.attrs['variable_name'] = param
             df.attrs['variable_units'] = ds[param].units
 
+            # add information about the location
+            df.attrs['distance_to_grid_point'] = ngcdist
+            df.attrs['lon_grid_point'] = ds.XLONG.to_numpy()[0, ngcind[0], ngcind[1]]
+            df.attrs['lat_grid_point'] = ds.XLAT.to_numpy()[0, ngcind[0], ngcind[1]]
+
+            # terrain elevation
+            wrf_hgt = ds.HGT[0,:,:]
+            
+            # col_names have to be returned that the number of returned values stays same
+            col_names = []
+            
+            return df, wrf_hgt, col_names
+        
+        # if a radius is given
+        else:        
+            # find grid cells that lie inside specified radius
+            gcind_inrad = grid.find_grid_cells_in_radius(ngcind, ngcdist, rad, lon, lat, ds)
+            
+            # check if variable is 3-dim
+            # variable is 3-dim if dimensions of dataframe variable >= 4
+            if len(ds[param].dims)>=4:
+                
+                # find nearest vertical level
+                nlind, nlhgt = grid.find_nearest_vlevel(
+                           ds[['PHB', 'PH', 'HGT', param]], ngcind, param, zagl)
+    
+                # extract time series
+                if param == 'T':
+                    # for 3-dim varibale T (perturbation potential Temperature) add 300 to get potential Temperature
+                    # create an array with values equal to zero 
+                    # with shape number of grids inside radius x 36 (time) x 36 (bottom_top)
+                    vararray = np.zeros((len(gcind_inrad),36,36))
+                    n = 0
+                    # Loop to write values of each grid in an array
+                    for i in gcind_inrad:
+                        vararray[n] = ds[param][np.arange(len(ds.Time)), nlind, i[0], i[1]] + 300
+                        n = n + 1
+                else:
+                    # for all other 4-dim variables 
+                    vararray = np.zeros((len(gcind_inrad),36,36))
+                    n = 0
+    
+                    for i in gcind_inrad:
+                        vararray[n] = ds[param][np.arange(len(ds.Time)), nlind, i[0], i[1]]
+                        n = n + 1
+                
+                # create column lables needed for DataFrame
+                col_names = [param]
+                for i in range(1, len(gcind_inrad)):
+                    col_names = col_names + [f'{param}{i}'] 
+                    
+                # write for each Grid the variables at hight level 0 to one DataFrame
+                df = pd.DataFrame()
+                
+                for i in range(len(gcind_inrad)):
+                    dfx = pd.DataFrame(data=vararray[i,:,0],
+                                       index=wrf_time, 
+                                       columns=[col_names[i]], dtype=None, copy=None)
+                    df = pd.concat([df,dfx],axis=1)
+    
+                # add information about the variable
+                df.attrs['variable_name'] = param
+                df.attrs['variable_units'] = ds[param].units
+    
+                # add information about the location
+                # information about location that changes for every gridcell
+                n = 0
+                for i in col_names:
+                    df[i].attrs['distance_to_grid_point'] = grid.haversine(lon,lat,
+                                                                           ds.XLONG[0,gcind_inrad[n,0],gcind_inrad[n,1]],
+                                                                           ds.XLAT[0,gcind_inrad[n,0],gcind_inrad[n,1]])
+                    df[i].attrs['lon_grid_point'] = ds.XLONG.to_numpy()[0,gcind_inrad[n,0],gcind_inrad[n,1]]
+                    df[i].attrs['lat_grid_point'] = ds.XLAT.to_numpy()[0, gcind_inrad[n,0], gcind_inrad[n,1]]
+                    n = n + 1
+                # information about location that stays the same for all gridcells
+                df.attrs['grid_point_elevation_time0'] = nlhgt[0]
+                
+                # terrain elevation
+                wrf_hgt = ds.HGT[0,:,:]
+                return df, wrf_hgt, col_names
+                
+            # if variable is 2D
+            # extract time series
+            vararray = np.zeros((len(gcind_inrad),36,36))
+            n = 0
+    
+            for i in gcind_inrad:
+                vararray[n] = ds[param][np.arange(len(ds.Time)), i[0], i[1]]
+                n = n + 1
+             
+            # create column lables needed for DataFrame
+            col_names = [param]
+            for i in range(1, len(gcind_inrad)):
+                col_names = col_names + [f'{param}{i}']   
+             
+            # write for each Grid the variables at hight level 0 to one DataFrame
+            df = pd.DataFrame()
+            
+            for i in range(len(gcind_inrad)):
+                dfx = pd.DataFrame(data=vararray[i,0,:],
+                                   index=wrf_time, 
+                                   columns=[col_names[i]], dtype=None, copy=None)
+                df = pd.concat([df,dfx],axis=1)
+                
+            # add information about the variable
+            df.attrs['variable_name'] = param
+            df.attrs['variable_units'] = ds[param].units
+    
             # add information about the location
             # information about location that changes for every gridcell
             n = 0
@@ -110,65 +209,9 @@ def get_wrf_timeseries(param, lon, lat, zagl, rad):
                 df[i].attrs['lon_grid_point'] = ds.XLONG.to_numpy()[0,gcind_inrad[n,0],gcind_inrad[n,1]]
                 df[i].attrs['lat_grid_point'] = ds.XLAT.to_numpy()[0, gcind_inrad[n,0], gcind_inrad[n,1]]
                 n = n + 1
-            # information about location that stays the same for all gridcells
-            df.attrs['grid_point_elevation_time0'] = nlhgt[0]
             
             # terrain elevation
             wrf_hgt = ds.HGT[0,:,:]
-            return df, wrf_hgt, col_names
-            
-        # if variable is not 2D
-        # convert binary times to datetime
-        wrf_time = pd.to_datetime(
-                   [bytes.decode(time) for time in ds.Times.data], 
-                   format='%Y-%m-%d_%H:%M:%S') 
-        # replace time coordinate (1-len(time)) with datetime times
-        ds = ds.assign_coords({'Time': wrf_time})
-
-        # extract time series
-        vararray = np.zeros((len(gcind_inrad),36,36))
-        n = 0
-
-        for i in gcind_inrad:
-            vararray[n] = ds[param][np.arange(len(ds.Time)), nlind, i[0], i[1]]
-            n = n + 1
-         
-        # create column lables needed for DataFrame
-        col_names = []
-        for i in range(len(gcind_inrad)):
-             col_names = col_names + [f'Grid{i}']   
-         
-        # write for each Grid the variables at hight level 0 to one DataFrame
-        df = pd.DataFrame()
-        
-        for i in range(len(gcind_inrad)):
-            dfx = pd.DataFrame(data=vararray[i,:,0],
-                               index=["1","2", "3","4","5","6","7","8","9","10"
-                                      ,"11","12","13","14","15","16","17","18","19","20"
-                                      ,"21","22","23","24","25","26","27","28","29","30"
-                                      ,"31","32","33","34","35","36"], 
-                               columns=[col_names[i]], dtype=None, copy=None)
-            df = pd.concat([df,dfx],axis=1)
-            
-        # add information about the variable
-        df.attrs['variable_name'] = param
-        df.attrs['variable_units'] = ds[param].units
-
-        # add information about the location
-        # information about location that changes for every gridcell
-        n = 0
-        for i in col_names:
-            df[i].attrs['distance_to_grid_point'] = grid.haversine(lon,lat,
-                                                                   ds.XLONG[0,gcind_inrad[n,0],gcind_inrad[n,1]],
-                                                                   ds.XLAT[0,gcind_inrad[n,0],gcind_inrad[n,1]])
-            df[i].attrs['lon_grid_point'] = ds.XLONG.to_numpy()[0,gcind_inrad[n,0],gcind_inrad[n,1]]
-            df[i].attrs['lat_grid_point'] = ds.XLAT.to_numpy()[0, gcind_inrad[n,0], gcind_inrad[n,1]]
-            n = n + 1
-        # information about location that stays the same for all gridcells
-        df.attrs['grid_point_elevation_time0'] = nlhgt[0]
-        
-        # terrain elevation
-        wrf_hgt = ds.HGT[0,:,:]
 
     return df, wrf_hgt, col_names
 
@@ -198,7 +241,7 @@ def mkdir(path, reset=False):
     return path
 
 
-def write_html(param, lon, lat, zagl, rad, directory=None):
+def write_html(param, lon, lat, zagl, rad = 0, directory=None):
     """ Create HTML with WRF plot 
     
     Returns
@@ -214,17 +257,20 @@ def write_html(param, lon, lat, zagl, rad, directory=None):
 
     # extract timeseries from WRF output
     print('Extracting timeseries at nearest grid cell')
-    df, hgt = get_wrf_timeseries(param, lon, lat, zagl, rad)
+    df, hgt, col_names = get_wrf_timeseries(param, lon, lat, zagl, rad)
    
     print('Plotting data')
     # plot the timeseries
     png = os.path.join(directory, 'timeseries.png')
-    graphics.plot_ts(df, filepath=png)
+    graphics.plot_ts(df,col_names, filepath=png)
 
     # plot a topography map
     png = os.path.join(directory, 'topography.png')
-    graphics.plot_topo(hgt, (df.attrs['lon_grid_point'], 
-                       df.attrs['lat_grid_point']), filepath=png)
+    for i in col_names:
+        graphics.plot_topo(hgt, (df[param].attrs['lon_grid_point'], 
+                       df[i].attrs['lat_grid_point']), filepath=png)
+    #graphics.plot_topo(hgt, (df.attrs['lon_grid_point'], 
+     #                  df.attrs['lat_grid_point']), filepath=png)
 
     # create HTML from template
     outpath = os.path.join(directory, 'index.html')
@@ -251,16 +297,14 @@ def skewT_html(lon, lat, time_index, directory=None):
     """
     
     #extract vertical profile for skewT 
-    try:
-        print('Extracting  vertical profile for skewT')
-        df_skewT, zlev, lcl_pressure, lcl_temperature,lfc_pressure, lfc_temperature = skewT.skewT_and_MSEDplot_dataframe(lon,lat,time_index)
-    except:
-        print('extraction determinated')
+    print('Extracting  vertical profile for skewT')
+    df_skewT, zlev, lcl_pressure, lcl_temperature,lfc_pressure, lfc_temperature = skewT.skewT_dataframe(lon,lat,time_index)
     
     # plot the skewT
-    print('plotting skewT') 
+    print('plotting skewT')
+    
     png = os.path.join('skewT.png')
-    skewT.skewT_and_MSED_plot(df_skewT, df_skewT['P'], df_skewT['Temperature_in_degC'], df_skewT['dewpoint'], df_skewT['U'], df_skewT['V'], lcl_pressure, lcl_temperature, lfc_pressure, lfc_temperature, df_skewT['QVAPOR'], zlev, filepath ='skewT.png')
+    skewT.skewT_plot(df_skewT, df_skewT['P'], df_skewT['Temperature_in_degC'], df_skewT['dewpoint'], df_skewT['U'], df_skewT['V'], lcl_pressure, lcl_temperature, lfc_pressure, lfc_temperature, df_skewT['QVAPOR'], zlev, filepath ='skewT.png')
     
     #create HTML from template
     outpath = os.path.join(directory, 'index.html')
