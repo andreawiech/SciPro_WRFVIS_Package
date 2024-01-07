c569a071800c3f262f3283fc8365e7b0870a2e92
Malte Hildebrandt <153686205+MalteHildebrandtDe@users.noreply.github.com>
2024-01-07 16:24:12 +0100

    Update core.py
    pep8 
    didnt know how to change the length of line 222 (please fix it)
    tried it and got an error

--- core.py
+++ core.py
@@ -47,7 +47,6 @@ Usage
 3. Ensure that the required dependencies are installed before using the module.
 """
 
-
 import os
 from tempfile import mkdtemp
 import shutil
@@ -59,7 +58,8 @@ import xarray as xr
 from wrfvis import cfg, grid, graphics, skewT, tables
 
 def get_wrf_timeseries(param, lon, lat, zagl, rad = 0):
-    """Read the time series from the WRF output file.
+    """
+    Read the time series from the WRF output file.
     
     Parameters
     ----------
@@ -104,13 +104,16 @@ def get_wrf_timeseries(param, lon, lat, zagl, rad = 0):
                 
                 # find nearest vertical level
                 nlind, nlhgt = grid.find_nearest_vlevel(
-                               ds[['PHB', 'PH', 'HGT', param]], ngcind, param, zagl)
+                               ds[['PHB', 'PH', 'HGT', param]], ngcind,
+                               param, zagl)
                 # extract time series
                 if param == 'T':
                     # WRF output is perturbation potential temperature
-                    vararray = ds[param][np.arange(len(ds.Time)), nlind, ngcind[0], ngcind[1]] + 300
+                    vararray = ds[param][np.arange(len(ds.Time)), nlind,
+                                         ngcind[0], ngcind[1]] + 300
                 else:
-                    vararray = ds[param][np.arange(len(ds.Time)), nlind, ngcind[0], ngcind[1]]
+                    vararray = ds[param][np.arange(len(ds.Time)),
+                                         nlind, ngcind[0], ngcind[1]]
                 df = vararray[:,0].to_dataframe()
     
                 # add information about the variable
@@ -119,8 +122,10 @@ def get_wrf_timeseries(param, lon, lat, zagl, rad = 0):
     
                 # add information about the location
                 df.attrs['distance_to_grid_point'] = ngcdist
-                df.attrs['lon_grid_point'] = ds.XLONG.to_numpy()[0, ngcind[0], ngcind[1]]
-                df.attrs['lat_grid_point'] = ds.XLAT.to_numpy()[0, ngcind[0], ngcind[1]]
+                df.attrs['lon_grid_point'] = ds.XLONG.to_numpy()[0, ngcind[0],
+                                                                 ngcind[1]]
+                df.attrs['lat_grid_point'] = ds.XLAT.to_numpy()[0, ngcind[0], 
+                                                                ngcind[1]]
                 df.attrs['grid_point_elevation_time0'] = nlhgt[0]
     
                 # terrain elevation
@@ -141,8 +146,10 @@ def get_wrf_timeseries(param, lon, lat, zagl, rad = 0):
 
             # add information about the location
             df.attrs['distance_to_grid_point'] = ngcdist
-            df.attrs['lon_grid_point'] = ds.XLONG.to_numpy()[0, ngcind[0], ngcind[1]]
-            df.attrs['lat_grid_point'] = ds.XLAT.to_numpy()[0, ngcind[0], ngcind[1]]
+            df.attrs['lon_grid_point'] = ds.XLONG.to_numpy()[0, ngcind[0],
+                                                             ngcind[1]]
+            df.attrs['lat_grid_point'] = ds.XLAT.to_numpy()[0, ngcind[0],
+                                                            ngcind[1]]
 
             # terrain elevation
             wrf_hgt = ds.HGT[0,:,:]
@@ -155,7 +162,8 @@ def get_wrf_timeseries(param, lon, lat, zagl, rad = 0):
         # if a radius is given
         else:        
             # find grid cells that lie inside specified radius
-            gcind_inrad = grid.find_grid_cells_in_radius(ngcind, ngcdist, rad, lon, lat, ds)
+            gcind_inrad = grid.find_grid_cells_in_radius(ngcind, ngcdist, rad,
+                                                         lon, lat, ds)
             
             # check if variable is 3-dim
             # variable is 3-dim if dimensions of dataframe variable >= 4
@@ -167,23 +175,27 @@ def get_wrf_timeseries(param, lon, lat, zagl, rad = 0):
     
                 # extract time series
                 if param == 'T':
-                    # for 3-dim varibale T (perturbation potential Temperature) add 300 to get potential Temperature
+                    # for 3-dim varibale T (perturbation potential Temperature) 
+                    # add 300 to get potential Temperature
                     # create an array with values equal to zero 
-                    # with shape number of grids inside radius x 36 (time) x 36 (bottom_top)
+                    # with shape number of grids inside radius x 36 (time) x 36
+                    # (bottom_top)
                     vararray = np.zeros((len(gcind_inrad),36,36))
                     n = 0
                     # Loop to write values of each grid in an array
                     for i in gcind_inrad:
-                        vararray[n] = ds[param][np.arange(len(ds.Time)), nlind, i[0], i[1]] + 300
-                        n = n + 1
+                        vararray[n] = ds[param][np.arange(len(ds.Time)), nlind,
+                                                i[0], i[1]] + 300
+                        n += 1
                 else:
                     # for all other 4-dim variables 
                     vararray = np.zeros((len(gcind_inrad),36,36))
                     n = 0
     
                     for i in gcind_inrad:
-                        vararray[n] = ds[param][np.arange(len(ds.Time)), nlind, i[0], i[1]]
-                        n = n + 1
+                        vararray[n] = ds[param][np.arange(len(ds.Time)), nlind,
+                                                i[0], i[1]]
+                        n += 1
                 
                 # create column lables needed for DataFrame
                 col_names = [param]
@@ -212,7 +224,7 @@ def get_wrf_timeseries(param, lon, lat, zagl, rad = 0):
                                                                            ds.XLAT[0,gcind_inrad[n,0],gcind_inrad[n,1]])
                     df[i].attrs['lon_grid_point'] = ds.XLONG.to_numpy()[0,gcind_inrad[n,0],gcind_inrad[n,1]]
                     df[i].attrs['lat_grid_point'] = ds.XLAT.to_numpy()[0, gcind_inrad[n,0], gcind_inrad[n,1]]
-                    n = n + 1
+                    n += 1
                 # information about location that stays the same for all gridcells
                 df.attrs['grid_point_elevation_time0'] = nlhgt[0]
                 
@@ -227,7 +239,7 @@ def get_wrf_timeseries(param, lon, lat, zagl, rad = 0):
     
             for i in gcind_inrad:
                 vararray[n] = ds[param][np.arange(len(ds.Time)), i[0], i[1]]
-                n = n + 1
+                n += 1
              
             # create column lables needed for DataFrame
             col_names = [param]
@@ -256,7 +268,7 @@ def get_wrf_timeseries(param, lon, lat, zagl, rad = 0):
                                                                        ds.XLAT[0,gcind_inrad[n,0],gcind_inrad[n,1]])
                 df[i].attrs['lon_grid_point'] = ds.XLONG.to_numpy()[0,gcind_inrad[n,0],gcind_inrad[n,1]]
                 df[i].attrs['lat_grid_point'] = ds.XLAT.to_numpy()[0, gcind_inrad[n,0], gcind_inrad[n,1]]
-                n = n + 1
+                n += 1
             
             # terrain elevation
             wrf_hgt = ds.HGT[0,:,:]
@@ -265,7 +277,8 @@ def get_wrf_timeseries(param, lon, lat, zagl, rad = 0):
 
 
 def mkdir(path, reset=False):
-    """Check if directory exists and if not, create one.
+    """
+    Check if directory exists and if not, create one.
         
     Parameters
     ----------
@@ -291,7 +304,7 @@ def mkdir(path, reset=False):
 
 def write_html(param, lon, lat, zagl, rad=0, directory=None):
     """ 
-    @ authors: Manuela Lehner, Andrea Wiech, Malte Hildebrandt
+    @ authors: Manuela Lehner, Malte Hildebrandt
     
     Create HTML with WRF plot 
     
@@ -322,8 +335,6 @@ def write_html(param, lon, lat, zagl, rad=0, directory=None):
     for i in col_names:
         graphics.plot_topo(hgt, (df[param].attrs['lon_grid_point'], 
                        df[i].attrs['lat_grid_point']), filepath=png_topo)
-    #graphics.plot_topo(hgt, (df.attrs['lon_grid_point'], 
-     #                  df.attrs['lat_grid_point']), filepath=png)
 
     # create HTML from template
     outpath = os.path.join(directory, 'topo_ts.html')
@@ -346,7 +357,7 @@ def write_html(param, lon, lat, zagl, rad=0, directory=None):
 
 def skewT_html(lon, lat, time_index, directory=None):
     """ 
-    @ authors: Manuela Lehner, Malte Hildebrandt, Matilda Achaab
+    @ authors: Malte Hildebrandt, Andrea Wiech
     
     Create HTML with SkewT and MSE plots 
     
@@ -365,20 +376,26 @@ def skewT_html(lon, lat, time_index, directory=None):
 
     # extract vertical profile for SkewT and MSE 
     print('Extracting vertical profile for SkewT and MSE')
-    df_skewT, zlev, lcl_pressure, lcl_temperature, lfc_pressure, lfc_temperature = skewT.skewt_and_mseplot_dataframe(lon, lat, time_index)
+    df_skewT, zlev, lcl_pressure, lcl_temperature, lfc_pressure, \
+        lfc_temperature = skewT.skewt_and_mseplot_dataframe(lon, lat,
+                                                            time_index)
 
     # plot the SkewT and MSE
     print('plotting skewT and SkewT')
-    path =['MSE.png','skewT.png']
+    path = ['MSE.png', 'skewT.png']
     for p in path:
         png = os.path.join(directory, p)
-        skewT.skewt_and_mseplot(df_skewT, df_skewT['P'], df_skewT['Temperature_in_degC'], df_skewT['dewpoint'],
-                                  df_skewT['U'], df_skewT['V'], lcl_pressure, lcl_temperature, lfc_pressure, lfc_temperature,
-                                  df_skewT['QVAPOR'], zlev, df_skewT['profile'],
-                                  filepath =png)
+        skewT.skewt_and_mseplot(df_skewT, df_skewT['P'],
+                                df_skewT['Temperature_in_degC'],
+                                df_skewT['dewpoint'],df_skewT['U'],
+                                df_skewT['V'], lcl_pressure, lcl_temperature,
+                                lfc_pressure, lfc_temperature,
+                                df_skewT['QVAPOR'], zlev, df_skewT['profile'],
+                                filepath=png)
         
     png_skewT = os.path.join(directory, 'skewT.png')
     png_MSE = os.path.join(directory, 'MSE.png')
+
     # create HTML from template
     outpath = os.path.join(directory, 'skewT_MSE.html')
     with open(cfg.html_skewT_template, 'r') as infile:
@@ -387,10 +404,12 @@ def skewT_html(lon, lat, time_index, directory=None):
         for txt in lines:
             if '[SKEWT_IMGPATH]' in txt and '[MSE_IMGPATH]' in txt:
                 # Add section for SkewT plot
-                txt = txt.replace('[SKEWT_IMGPATH]', os.path.relpath(png_skewT, directory))
+                txt = txt.replace('[SKEWT_IMGPATH]', os.path.relpath(png_skewT,
+                                                                     directory))
                 
                 # Add section for MSE plot
-                txt_mse = txt.replace('[MSE_IMGPATH]', os.path.relpath(png_MSE, directory))
+                txt_mse = txt.replace('[MSE_IMGPATH]',
+                                      os.path.relpath(png_MSE, directory))
                 
                 out.append(txt)
                 out.append(txt_mse)
@@ -406,7 +425,8 @@ def skewT_html(lon, lat, time_index, directory=None):
     return html_content, outpath
 
 
-def generate_combined_html(param, lon, lat, time_index, zagl, rad=0, directory=None):
+def generate_combined_html(param, lon, lat, time_index, zagl, rad=0,
+                           directory=None):
     """
     @ author: Malte Hildebrandt
     
@@ -441,52 +461,54 @@ def generate_combined_html(param, lon, lat, time_index, zagl, rad=0, directory=N
         # Generate plots
         # Extract HTML content and paths from functions
         print("Generating WRF timeseries plot...")
-        html_timeseries, path_timeseries = write_html(param, lon, lat, zagl, rad=rad, directory=directory)
+        html_timeseries, path_timeseries = write_html(param, lon, lat, zagl,
+                                                      rad=rad,
+                                                      directory=directory)
         print("Generating SkewT and MSE plots...")
-        html_skewT_mse, path_skewT_mse = skewT_html(lon, lat, time_index, directory=directory)
+        html_skewT_mse, path_skewT_mse = skewT_html(lon, lat, time_index,
+                                                    directory=directory)
 
         html_content = f"""
         <html>
         <head>
             <style>
                 body {{
-                    margin: 0; /* Remove default margin */
-                    padding: 0; /* Remove default padding */
+                    margin: 0;
+                    padding: 0;
                 }}
         
                 .flex-container {{
                     display: flex;
                     justify-content: space-between;
                     overflow-x: auto;
-                    flex-wrap: wrap; /* Allow items to wrap to the next line */
-                    border-bottom: 2px solid black; /* Add a black border at the bottom of each flex-container */
-                    padding-bottom: 10px; /* Add some spacing between sections */
-                    margin-bottom: 20px; /* Add more spacing between sections */
+                    flex-wrap: wrap;
+                    border-bottom: 2px solid black;
+                    padding-bottom: 10px;
+                    margin-bottom: 20px;
                 }}
-                
+        
                 .flex-item {{
-                    margin-right: 5px; /* Reduced margin between items */
+                    margin-right: 5px;
                     flex-wrap: wrap;
                     width: 150%;
                 }}
         
                 .flex-subitem {{
-                    margin-right: 5px; /* Reduced margin between items */
-                    width: 49%; /* Make each item take approximately half the width */
-                    box-sizing: border-box; /* Include padding and border in item's total width and height */
+                    margin-right: 5px;
+                    width: 49%;
+                    box-sizing: border-box;
                 }}
         
                 img {{
-                    max-width: 100%; /* Make sure images are responsive and don't exceed their container width */
-                    height: auto; /* Maintain image aspect ratio */
-                    display: block; /* Remove extra space below inline images */
-                    margin: 0 auto; /* Center images within their containers */
+                    max-width: 100%;
+                    height: auto;
+                    display: block;
+                    margin: 0 auto;
                 }}
         
-                /* Add some space between the tables and the text */
                 .table-description {{
                     margin-top: 20px;
-                    max-width: 100%; /* Adjust as needed to control the maximum width of the description */
+                    max-width: 100%;
                 }}
             </style>
         </head>
@@ -503,8 +525,9 @@ def generate_combined_html(param, lon, lat, time_index, zagl, rad=0, directory=N
                 <!-- Add a description under the geo table -->
                 <div class="table-description">
                     <p>
-                        This here is a text to test how text looks below the plot. 
-                        Later, some definitions of terms and similar information will be added.
+                        The Minimum Mountain Height in this table represents the
+                        calculated minimum elevation of mountains, and it is 
+                        contingent on their respective longitudes. 
                     </p>
                 </div>
             </div>
@@ -534,11 +557,11 @@ def generate_combined_html(param, lon, lat, time_index, zagl, rad=0, directory=N
         <div class="flex-container">
             <div class="flex-subitem">
                 <h3>SkewT Diagram</h3>
-                <img alt="no img" src="plot_skewT_skewT.png" width="680">
+                <img alt="no img" src="skewT.png" width="680">
             </div>
             <div class="flex-subitem">
                 <h3>MSE Diagram</h3>
-                <img alt="no img" src="plot_skewT_MSE.png" width="730">
+                <img alt="no img" src="MSE.png" width="730">
             </div>
         </div>
         
@@ -546,9 +569,11 @@ def generate_combined_html(param, lon, lat, time_index, zagl, rad=0, directory=N
         </html>
         """
 
+
         print("Saving HTML file...")
         # Save HTML file
-        html_filepath = os.path.join(directory, 'Visualization_of_WRF_model.html')
+        html_filepath = os.path.join(directory,
+                                     'Visualization_of_WRF_model.html')
         with open(html_filepath, 'w') as html_file:
             html_file.write(html_content)
 
