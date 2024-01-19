32375c553fd300a2223c7680f615ef67b0cf88ea
andreawiech <118971544+andreawiech@users.noreply.github.com>
2024-01-19 20:40:53 +0100

    Update setup.py
    Added a lot to the re_packages.
    
    And changed some other stuff like version, Copytight

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ We reproduce their license terms here:
 Copyright (c) 2016 The Python Packaging Authority (PyPA)
 Copyright (c) 2018 Fabien Maussion
 Copytight (c) 2023 Manuela Lehner
+Copytight (c) 2024 Mathilda Achaab, Malte Hildebrand, Joep Van Noort, Andrea Wiech
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this file (the "Software"), to deal in
@@ -56,7 +57,17 @@ req_packages = ['numpy',
                 'netCDF4',
                 'metpy',
                 'MSEplots-pkg',
-                'scikit-learn']
+                'scikit-learn',
+                'pytest',
+                'os',
+                'tempfile',
+                'shutil',
+                'sys',
+                'webbrowser',
+                're',
+                'setuptools',
+                'codecs',
+                'importlib']
 
 check_dependencies(req_packages)
 
@@ -89,7 +100,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',  # Required
+    version='0.0.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
