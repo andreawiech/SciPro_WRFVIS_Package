62cd0257819018b46432d32e7fb31cdcfe18965d
andreawiech <118971544+andreawiech@users.noreply.github.com>
2024-01-19 11:21:02 +0100

    19.01.24 updated cltools.py

--- cltools.py
+++ cltools.py
@@ -1,6 +1,7 @@
 """ contains command line tools of WRFvis
 
 Manuela Lehner
+Andrea Wiech
 November 2023
 """
 
@@ -55,33 +56,78 @@ def gridcell(args):
         print('wrfvis_gridcell: ' + wrfvis.__version__)
         print('Licence: public domain')
         print('wrfvis_gridcell is provided "as is", without warranty of any kind')
-    
+        
+    elif ('-p' in args) and ('-l' in args) and ('-t' in args):
+        # if command line arguments '-p', '-l'  and '-t' are entered, assign 
+        # the values to variablesand generate a html with all functionalities 
+        # of the package
+        param = args[args.index('-p') + 1]
+        lon = float(args[args.index('-l') + 1])
+        lat = float(args[args.index('-l') + 2])
+        zagl = float(args[args.index('-l') + 3])
+        timeindex = int(args[args.index('-t') + 1])
+        if '-r' in args:
+            rad = float(args[args.index('-r') + 1])
+            html_path = wrfvis.write_html(param, lon, lat, zagl, rad, timeindex)
+        else:
+            html_path = wrfvis.write_html(param, lon, lat, zagl, timeindex)
+        if '--no-browser' in args:
+            print('File successfully generated at: ' + html_path)
+        else:
+            webbrowser.get().open_new_tab('file://' + html_path)
+                
     elif ('-p' in args) and ('-l' in args):
-        # if command line arguments '-p', '-l' are entered, assign 
+        # if command line arguments '-p', '-l' are entered, assign
         # the values to variables
         param = args[args.index('-p') + 1]
         lon = float(args[args.index('-l') + 1])
         lat = float(args[args.index('-l') + 2])
         zagl = float(args[args.index('-l') + 3])
         if '-r' in args:
+            # if radius is entered call write_html_multiple_gridcell function 
+            # with radius
             rad = float(args[args.index('-r') + 1])
-            html_path = wrfvis.write_html(param, lon, lat, zagl, rad)
+            html_path = wrfvis.write_html_multiple_gridcell(param, lon, lat, zagl, rad)
         else:
-            html_path = wrfvis.write_html(param, lon, lat, zagl)
+            # if no radius is entered call write_html_multiple_gridcell function
+            # without radius
+            html_path = wrfvis.write_html_multiple_gridcell(param, lon, lat, zagl)
         if '--no-browser' in args:
             print('File successfully generated at: ' + html_path)
         else:
             webbrowser.get().open_new_tab('file://' + html_path)
-    # write an extra html for the skew T?
+            
+    
     elif  ('-l' in args) and  ('-t' in args):
+        # if command line arguments -l and -t are entered, assign
+        # the values to variables
         time_index = int(args[args.index('-t') + 1])
         lon = float(args[args.index('-l') + 1])
         lat = float(args[args.index('-l') + 2])
-        html_path = wrfvis.core.skewT_html(lon, lat, time_index)
+        #  write html for the skew T
+        html_path = wrfvis.write_html_skewT(lon, lat, time_index)
+        if '--no-browser' in args:
+            print('File successfully generated at: ' + html_path)
+        else:
+            webbrowser.get().open_new_tab('file://' + html_path)
+    
+    elif ('-l' in args):
+        # if command line arguments -l is entered, assign
+        # the values to variables
+        lon = float(args[args.index('-l') + 1])
+        lat = float(args[args.index('-l') + 2])
+        # write html for the snowcheck
+        html_path = wrfvis.write_html_snowcheck(lon, lat)
+        if '--no-browser' in args:
+            print('File successfully generated at: ' + html_path)
+        else:
+            webbrowser.get().open_new_tab('file://' + html_path)
+        
+
+        
     else:
         print('wrfvis_gridcell: command not understood. '
               'Type "wrfvis_gridcell --help" for usage information.')
-
 
 def wrfvis_gridcell():
     """Entry point for the wrfvis_gridcell application script"""
