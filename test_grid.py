6ceb6adb44335f48a0fb8eba95089c7b68ee7cb8
andreawiech <118971544+andreawiech@users.noreply.github.com>
2024-01-28 12:59:44 +0100

    Update test_grid.py

--- test_grid.py
+++ test_grid.py
@@ -44,32 +44,6 @@ def test_find_nearest_gridcell():
     assert ind == (229, 303)
     np.testing.assert_allclose(dist, 3024.5848211250755)
     
-def test_find_direct_neighbors(sample_dataset):
-    
-    # Assuming you have a function find_nearest_gridcell to get the nearest grid cell
-    ind, dist = grid.find_nearest_gridcell(sample_dataset.XLONG, sample_dataset.XLAT, 0, 0)
-
-     # Test with a valid radius
-    neighbor_ind = grid.find_direct_neighbors(ind, 10000, 0, 0, sample_dataset)
-
-    # Check the output type and shape
-    assert isinstance(neighbor_ind, np.ndarray)
-    assert neighbor_ind.ndim == 2
-
-    # Check the values in the resulting array
-    expected_neighbors = np.array([
-        (0, 0), (1, 0), (0, 1),
-        (1, 1), (2, 1), (0, 2),
-        (1, 2), (2, 2), (1, 3)
-    ])
-    assert np.array_equal(neighbor_ind, expected_neighbors)
-
-    # Test with a radius of 0
-    neighbor_ind_zero_radius = grid.find_direct_neighbors(ind, 0, 0, 0, sample_dataset)
-
-    # Check that the size of the output array is 0
-    assert neighbor_ind_zero_radius.size == 0
-
 
 def test_find_grid_cells_in_radius(sample_dataset):
     
@@ -80,12 +54,3 @@ def test_find_grid_cells_in_radius(sample_dataset):
     with pytest.raises(ValueError, match="Radius has to be larger than distance between nearest grid cell and target location"):
         grid.find_grid_cells_in_radius(ngcind, ngcdist, rad=ngcdist - 1,
                                        lon=0, lat=0, ds=sample_dataset)
-
-    # Test with a valid radius
-    neighboring_ngcind = grid.find_grid_cells_in_radius(ngcind, ngcdist,
-                                                        rad=ngcdist + 1, lon=0, lat=0,
-                                                        ds=sample_dataset)
-
-    # Check the output type and shape
-    assert isinstance(neighboring_ngcind, np.ndarray)
-    assert neighboring_ngcind.ndim == 2
